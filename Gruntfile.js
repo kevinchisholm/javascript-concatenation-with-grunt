f2e09190a9f4adfbf33692aa2a97a6baa454aca4
Kevin Chisholm <kevinchisholm@Kevins-MacBook-Pro.local>
2017-01-29 07:29:01 -0500

    Fix indenting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,17 @@
 module.exports = function(grunt) {
 
-// This is the configuration.
-grunt.initConfig({
-  concat: {
-    options: {
-      separator: ';',
+  // This is the configuration.
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['src/script-1.js', 'src/script-2.js', 'src/script-3.js'],
+        dest: 'dist/allScripts.js',
+      },
     },
-    dist: {
-      src: ['src/script-1.js', 'src/script-2.js', 'src/script-3.js'],
-      dest: 'dist/allScripts.js',
-    },
-  },
-});
+  });
 
   // Load the plugin that provides the "concat" task.
   grunt.loadNpmTasks('grunt-contrib-concat');
